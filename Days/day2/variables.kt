d2e4798992c61a7635c3ff3e1b0aeca32f8ce1b9
Felipe Alafy <f.alafyrsilva@hotmail.com>
2020-11-15 22:16:22 -0300

    day2 variables, constants and arrays

---
+++ Days/day2/variables.kt
@@ -0,0 +1,13 @@
+package Days.day2
+// Variaveis, constantes e um pouco de array, livro pagina 48 a 50
+fun main() {
+    val a = 42
+    var b = "Qual é a resposta para tudo?"
+    val languages = arrayListOf("Java")
+    languages.add("Kotlin")
+    languages.add("Python")
+
+    println(languages)
+    println(b)
+    println(a)
+}
