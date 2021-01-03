f526d8a711faac95b4a1aafa6a8ff80079aaecfb
Felipe Alafy <f.alafyrsilva@hotmail.com>
2021-01-03 09:10:10 -0300

    Dia 9 - Propriedades de extensão.

--- Days/day9/las.kt
+++ Days/day9/las.kt
@@ -1,4 +1,10 @@
 package Days.day9
 
-class las {
-}
+import java.lang.StringBuilder
+
+//LastChar
+var StringBuilder.lastChar:Char
+    get() = get(length - 1)
+    set(value) {
+        this.setCharAt(length - 1, value)
+    }
--- Days/day9/main.kt
+++ Days/day9/main.kt
@@ -1,2 +1,12 @@
 package Days.day9
 
+import java.lang.StringBuilder
+import Days.day9.lastChar
+
+fun main() {
+    var st = StringBuilder("String")
+    st.lastChar = '!'
+    println(st)
+}
+
+
